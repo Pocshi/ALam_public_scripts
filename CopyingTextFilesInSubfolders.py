1424701f6551a90b45c39de19a67ef6db38ece5c
Pocshi <39612097+Pocshi@users.noreply.github.com>
2019-10-21 13:23:44 +1100

    Descriptions within script
    Updated script to include descriptions of certain things.

--- CopyingTextFilesInSubfolders.py
+++ CopyingTextFilesInSubfolders.py
@@ -2,27 +2,31 @@ import os
 import csv
 import xlsxwriter
 from glob import glob
-from sys import exit
-
-text_file = 'ABC_*info'
-output_file = 'Report'
 
 
+#Searches name of text files
+text_file = 'Name_of_text_files'
 
-base_directory = '//fs2.shared.sydney.edu.au/BMRI/CRU/HBA/ABC_MRI/MRI_Data/preprocessed_data/dti'
+#name of the output csv file
+output_file = 'Name_of_output_file'
+
+
+#base directory that you are searching from
+base_directory = 'file_location'
+
 if base_directory:
 	subject_files = [os.path.join(root, subject_file) for root, dirs, files in os.walk(base_directory) for subject_file in files if subject_file.endswith((".txt")) if not os.path.basename(subject_file).startswith(('.','~'))]
-	print(subject_files)#[subject_file for subject_file in glob(os.path.join(base_directory, '**', text_file), recursive = True) if not os.path.basename(subject_file).startswith(('.','~'))]
+	print(subject_files) #this line will print out the filenames, remove this if you don't want the console to explode.
 	
 	if subject_files:
 		with open(os.path.join(base_directory, output_file + '.csv'), 'w', newline='') as output_file:
 			file_writer = csv.writer(output_file)
 			header = None
 			for subject_number, subject_file in enumerate(subject_files,1):
-				with open(subject_file, encoding='utf-8') as csv_file:
-					file_reader = csv.reader(csv_file,delimiter=':')
+				with open(subject_file, encoding='ansi') as csv_file: #encoding of the text file, change from ansi to another as required.
+					file_reader = csv.reader(csv_file,delimiter=':') #change delimiter depending on your file
 					for row in file_reader:
-						print(row)
+						print(row) #this will print out the files, remove this if you don't want the console to explode
 
-                    # Write values to new row, prepending with filename
+                    # write value to csv file
 					file_writer.writerow([os.path.basename(subject_file)] + list(row))
